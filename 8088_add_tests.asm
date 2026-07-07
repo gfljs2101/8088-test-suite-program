2148700821968e73459d4a53609bddc519d527e6
gfljs2101 <gfljs2100.user@gmail.com>
2026-07-07 09:59:48 +0800

    Add paged output routine and replace DOS prints with paged_print

--- 8088_add_tests.asm
+++ 8088_add_tests.asm
@@ -98,8 +98,7 @@ start:
 
     ; Done
     mov dx, msg_done
-    mov ah, 9
-    int 21h
+    call paged_print
 
     mov ax, 4C00h
     int 21h
@@ -134,8 +133,7 @@ test_add_rm8_r8:
 .t1_fail:
     mov dx, test1_fail
 .t1_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; Test 2: ADD r/m16, r16
@@ -157,8 +155,7 @@ test_add_rm16_r16:
 .t2_fail:
     mov dx, test2_fail
 .t2_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; Test 3: ADD r8, r/m8
@@ -179,8 +176,7 @@ test_add_r8_rm8:
 .t3_fail:
     mov dx, test3_fail
 .t3_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; Test 4: ADD r16, r/m16
@@ -201,8 +197,7 @@ test_add_r16_rmem:
 .t4_fail:
     mov dx, test4_fail
 .t4_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; Test 5: ADD AL, d8
@@ -222,8 +217,7 @@ test_add_al_d8:
 .t5_fail:
     mov dx, test5_fail
 .t5_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; Test 6: ADD AX, d16
@@ -243,8 +237,7 @@ test_add_ax_d16:
 .t6_fail:
     mov dx, test6_fail
 .t6_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; ---------------------------------------------------------------------------
@@ -270,8 +263,7 @@ test_or_rm8_r8:
 .or1_fail:
     mov dx, or1_fail
 .or1_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; OR r/m16, r16 (09 /r)
@@ -293,8 +285,7 @@ test_or_rm16_r16:
 .or2_fail:
     mov dx, or2_fail
 .or2_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; OR r8, r/m8 (0A /r)
@@ -315,8 +306,7 @@ test_or_r8_rm8:
 .or3_fail:
     mov dx, or3_fail
 .or3_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; OR r16, r/m16 (0B /r)
@@ -337,8 +327,7 @@ test_or_r16_rmem:
 .or4_fail:
     mov dx, or4_fail
 .or4_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; OR AL, imm8 (0C ib)
@@ -358,8 +347,7 @@ test_or_al_imm8:
 .or5_fail:
     mov dx, or5_fail
 .or5_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; OR AX, imm16 (0D iw)
@@ -379,8 +367,7 @@ test_or_ax_imm16:
 .or6_fail:
     mov dx, or6_fail
 .or6_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; ---------------------------------------------------------------------------
@@ -399,8 +386,7 @@ test_push_es:
 .pesh_fail:
     mov dx, pesh_fail
 .pesh_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; POP ES (0x07) - push a value then pop ES
@@ -416,8 +402,7 @@ test_pop_es:
 .pes_fail:
     mov dx, pes_fail
 .pes_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; PUSH CS (0x0E) - push CS then pop AX and compare
@@ -432,8 +417,7 @@ test_push_cs:
 .pcsh_fail:
     mov dx, pcsh_fail
 .pcsh_print:
-    mov ah, 9
-    int 21h
+    call paged_print
     ret
 
 ; ---------------------------------------------------------------------------
@@ -453,8 +437,7 @@ test_adc_rm8_r8:
 .adc1_fail:
     mov dx, adc1_fail
 .adc1_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -472,8 +455,7 @@ test_adc_rm16_r16:
 .adc2_fail:
     mov dx, adc2_fail
 .adc2_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -490,8 +472,7 @@ test_adc_r8_rm8:
 .adc3_fail:
     mov dx, adc3_fail
 .adc3_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -508,8 +489,7 @@ test_adc_r16_rmem:
 .adc4_fail:
     mov dx, adc4_fail
 .adc4_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -525,8 +505,7 @@ test_adc_al_d8:
 .adc5_fail:
     mov dx, adc5_fail
 .adc5_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -542,8 +521,7 @@ test_adc_ax_d16:
 .adc6_fail:
     mov dx, adc6_fail
 .adc6_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -561,8 +539,7 @@ test_sbb_rm8_r8:
 .sbb1_fail:
     mov dx, sbb1_fail
 .sbb1_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -580,8 +557,7 @@ test_sbb_rm16_r16:
 .sbb2_fail:
     mov dx, sbb2_fail
 .sbb2_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -598,8 +574,7 @@ test_sbb_r8_rm8:
 .sbb3_fail:
     mov dx, sbb3_fail
 .sbb3_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -616,8 +591,7 @@ test_sbb_r16_rmem:
 .sbb4_fail:
     mov dx, sbb4_fail
 .sbb4_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -633,8 +607,7 @@ test_sbb_al_d8:
 .sbb5_fail:
     mov dx, sbb5_fail
 .sbb5_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -650,8 +623,7 @@ test_sbb_ax_d16:
 .sbb6_fail:
     mov dx, sbb6_fail
 .sbb6_print:
-    mov ah,9
-    int 21h
+    call paged_print
     clc
     ret
 
@@ -668,8 +640,7 @@ test_and_rm8_r8:
 .and1_fail:
     mov dx, and1_fail
 .and1_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; AND r/m16, r16 (21 /r)
@@ -685,8 +656,7 @@ test_and_rm16_r16:
 .and2_fail:
     mov dx, and2_fail
 .and2_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; AND r8, r/m8 (22 /r)
@@ -701,8 +671,7 @@ test_and_r8_rm8:
 .and3_fail:
     mov dx, and3_fail
 .and3_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; AND r16, r/m16 (23 /r)
@@ -717,8 +686,7 @@ test_and_r16_rmem:
 .and4_fail:
     mov dx, and4_fail
 .and4_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; AND AL, imm8 (24 ib)
@@ -732,8 +700,7 @@ test_and_al_imm8:
 .and5_fail:
     mov dx, and5_fail
 .and5_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; AND AX, imm16 (25 iw)
@@ -747,8 +714,7 @@ test_and_ax_imm16:
 .and6_fail:
     mov dx, and6_fail
 .and6_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; DAA (27)
@@ -763,8 +729,7 @@ test_daa:
 .daa_fail:
     mov dx, daa_fail
 .daa_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; DAS (2F)
@@ -782,8 +747,7 @@ test_das:
 .das_fail:
     mov dx, das_fail
 .das_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; SUB r/m8, r8 (28 /r)
@@ -799,8 +763,7 @@ test_sub_rm8_r8:
 .sub1_fail:
     mov dx, sub1_fail
 .sub1_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; SUB r/m16, r16 (29 /r)
@@ -816,8 +779,7 @@ test_sub_rm16_r16:
 .sub2_fail:
     mov dx, sub2_fail
 .sub2_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; SUB r8, r/m8 (2A /r)
@@ -832,8 +794,7 @@ test_sub_r8_rm8:
 .sub3_fail:
     mov dx, sub3_fail
 .sub3_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; SUB r16, r/m16 (2B /r)
@@ -848,8 +809,7 @@ test_sub_r16_rmem:
 .sub4_fail:
     mov dx, sub4_fail
 .sub4_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; SUB AL, imm8 (2C ib)
@@ -863,8 +823,7 @@ test_sub_al_d8:
 .sub5_fail:
     mov dx, sub5_fail
 .sub5_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; SUB AX, imm16 (2D iw)
@@ -878,8 +837,7 @@ test_sub_ax_d16:
 .sub6_fail:
     mov dx, sub6_fail
 .sub6_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; XOR r/m8, r8 (30 /r)
@@ -895,8 +853,7 @@ test_xor_rm8_r8:
 .xor1_fail:
     mov dx, xor1_fail
 .xor1_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; XOR r/m16, r16 (31 /r)
@@ -912,8 +869,7 @@ test_xor_rm16_r16:
 .xor2_fail:
     mov dx, xor2_fail
 .xor2_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; XOR r8, r/m8 (32 /r)
@@ -928,8 +884,7 @@ test_xor_r8_rm8:
 .xor3_fail:
     mov dx, xor3_fail
 .xor3_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; XOR r16, r/m16 (33 /r)
@@ -944,8 +899,7 @@ test_xor_r16_rmem:
 .xor4_fail:
     mov dx, xor4_fail
 .xor4_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; XOR AL, imm8 (34 ib)
@@ -959,8 +913,7 @@ test_xor_al_imm8:
 .xor5_fail:
     mov dx, xor5_fail
 .xor5_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; XOR AX, imm16 (35 iw)
@@ -974,8 +927,7 @@ test_xor_ax_imm16:
 .xor6_fail:
     mov dx, xor6_fail
 .xor6_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; CMP r/m8, r8 (38 /r)
@@ -989,8 +941,7 @@ test_cmp_rm8_r8:
 .cmp1_pass:
     mov dx, cmp1_pass
 .cmp1_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
 ; CMP r/m16, r16 (39 /r)
@@ -1004,172 +955,53 @@ test_cmp_rm16_r16:
 .cmp2_pass:
     mov dx, cmp2_pass
 .cmp2_print:
-    mov ah,9
-    int 21h
+    call paged_print
     ret
 
-; CMP r8, r/m8 (3A /r)
-test_cmp_r8_rm8:
-    mov byte [cmp_rm8_b], 0Fh
-    mov al, 0Fh
-    cmp al, [cmp_rm8_b]
-    je .cmp3_pass
-    mov dx, cmp3_fail
-    jmp .cmp3_print
-.cmp3_pass:
-    mov dx, cmp3_pass
-.cmp3_print:
-    mov ah,9
-    int 21h
-    ret
-
-; CMP r16, r/m16 (3B /r)
-test_cmp_r16_rmem:
-    mov word [cmp_rm16_b], 0FFFFh
-    mov ax, 0FFFFh
-    cmp ax, [cmp_rm16_b]
-    je .cmp4_pass
-    mov dx, cmp4_fail
-    jmp .cmp4_print
-.cmp4_pass:
-    mov dx, cmp4_pass
-.cmp4_print:
-    mov ah,9
-    int 21h
-    ret
-
-; CMP AL, imm8 (3C ib)
-test_cmp_al_d8:
-    mov al, 0Fh
-    cmp al, 0Fh
-    je .cmp5_pass
-    mov dx, cmp5_fail
-    jmp .cmp5_print
-.cmp5_pass:
-    mov dx, cmp5_pass
-.cmp5_print:
-    mov ah,9
-    int 21h
-    ret
-
-; CMP AX, imm16 (3D iw)
-test_cmp_ax_d16:
-    mov ax, 0FFFFh
-    cmp ax, 0FFFFh
-    je .cmp6_pass
-    mov dx, cmp6_fail
-    jmp .cmp6_print
-.cmp6_pass:
-    mov dx, cmp6_pass
-.cmp6_print:
-    mov ah,9
-    int 21h
-    ret
-
-; PUSH SS (0x16) - push SS then pop AX to verify value
-test_push_ss:
-    mov ax, 05555h
-    mov ss, ax
-    push ss
-    pop ax
-    cmp ax, 05555h
-    jne .pssh_fail
-    mov dx, pssh_pass
-    jmp .pssh_print
-.pssh_fail:
-    mov dx, pssh_fail
-.pssh_print:
-    mov ah,9
-    int 21h
-    ret
-
-; POP SS (0x17) - push a value then pop SS
-test_pop_ss:
-    mov ax, 0AA55h
+; ---------------------------------------------------------------------------
+; Paged print routine and paging data
+paged_print:
     push ax
-    pop ss
-    mov ax, ss
-    cmp ax, 0AA55h
-    jne .pss_fail
-    mov dx, pss_pass
-    jmp .pss_print
-.pss_fail:
-    mov dx, pss_fail
-.pss_print:
-    mov ah,9
-    int 21h
-    ret
+    push bx
+    push cx
+    push dx
+    push si
+    push di
 
-; PUSH DS (0x1E) - push DS then pop AX to verify value
-test_push_ds:
-    mov ax, 03333h
-    mov ds, ax
-    push ds
-    pop ax
-    cmp ax, 03333h
-    jne .pdsh_fail
-    mov dx, pdsh_pass
-    jmp .pdsh_print
-.pdsh_fail:
-    mov dx, pdsh_fail
-.pdsh_print:
-    mov ah,9
+    ; print the string pointed by DX using DOS function 9
+    mov ah, 9
     int 21h
-    ret
 
-; POP DS (0x1F) - push a value then pop DS
-test_pop_ds:
-    mov ax, 07777h
-    push ax
-    pop ds
-    mov ax, ds
-    cmp ax, 07777h
-    jne .pds_fail
-    mov dx, pds_pass
-    jmp .pds_print
-.pds_fail:
-    mov dx, pds_fail
-.pds_print:
-    mov ah,9
+    ; increment printed-line counter
+    inc word [page_count]
+    ; compare with limit (25)
+    mov ax, [page_count]
+    cmp ax, 25
+    jb .paged_done
+
+    ; print prompt "scroll?" and wait for any key
+    mov dx, scroll_prompt
+    mov ah, 9
     int 21h
-    ret
-
-; AAA (37)
-test_aaa:
+    ; wait for any key (INT 16h AH=0)
     mov ah, 0
-    mov al, 09h
-    add al, 1
-    aaa
-    cmp al, 0
-    jne .aaa_fail
-    cmp ah, 1
-    jne .aaa_fail
-    mov dx, aaa_pass
-    jmp .aaa_print
-.aaa_fail:
-    mov dx, aaa_fail
-.aaa_print:
-    mov ah,9
-    int 21h
-    ret
+    int 16h
 
-; AAS (3F)
-test_aas:
-    mov ah, 02h
-    mov al, 0
-    sub al, 1
-    aas
-    cmp al, 09h
-    jne .aas_fail
-    cmp ah, 01h
-    jne .aas_fail
-    mov dx, aas_pass
-    jmp .aas_print
-.aas_fail:
-    mov dx, aas_fail
-.aas_print:
-    mov ah,9
+    ; optional new line after key
+    mov dx, scroll_nl
+    mov ah, 9
     int 21h
+
+    ; reset counter
+    mov word [page_count], 0
+
+.paged_done:
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
     ret
 
 ; ---------------------------------------------------------------------------
@@ -1184,35 +1016,10 @@ or_rm8_b:    db 0
 or_rm16_a:   dw 0
 or_rm16_b:   dw 0
 
-adc_rm8:     db 0
-adc_rm16:    dw 0
-adc_r8_src:  db 0
-adc_r16_src: dw 0
-
-sbb_rm8:     db 0
-sbb_rm16:    dw 0
-sbb_r8_src:  db 0
-sbb_r16_src: dw 0
-
-and_rm8_a:   db 0
-and_rm8_b:   db 0
-and_rm16_a:  dw 0
-and_rm16_b:  dw 0
-
-sub_rm8:     db 0
-sub_rm16:    dw 0
-sub_r8_src:  db 0
-sub_r16_src: dw 0
-
-xor_rm8_a:   db 0
-xor_rm8_b:   db 0
-xor_rm16_a:  dw 0
-xor_rm16_b:  dw 0
-
-cmp_rm8_a:   db 0
-cmp_rm8_b:   db 0
-cmp_rm16_a:  dw 0
-cmp_rm16_b:  dw 0
+; paging counter and prompt
+page_count:  dw 0
+scroll_prompt: db 'scroll?',0Dh,0Ah,'$'
+scroll_nl:   db 0Dh,0Ah,'$'
 
 ; ADD test strings
 test1_pass:  db 'ADD r/m8, r8: PASS',0Dh,0Ah,'$'
@@ -1238,168 +1045,3 @@ or1_pass:    db 'OR r/m8, r8: PASS',0Dh,0Ah,'$'
 or1_fail:    db 'OR r/m8, r8: FAIL',0Dh,0Ah,'$'
 
 or2_pass:    db 'OR r/m16, r16: PASS',0Dh,0Ah,'$'
-or2_fail:    db 'OR r/m16, r16: FAIL',0Dh,0Ah,'$'
-
-or3_pass:    db 'OR r8, r/m8: PASS',0Dh,0Ah,'$'
-or3_fail:    db 'OR r8, r/m8: FAIL',0Dh,0Ah,'$'
-
-or4_pass:    db 'OR r16, r/m16: PASS',0Dh,0Ah,'$'
-or4_fail:    db 'OR r16, r/m16: FAIL',0Dh,0Ah,'$'
-
-or5_pass:    db 'OR AL, imm8: PASS',0Dh,0Ah,'$'
-or5_fail:    db 'OR AL, imm8: FAIL',0Dh,0Ah,'$'
-
-or6_pass:    db 'OR AX, imm16: PASS',0Dh,0Ah,'$'
-or6_fail:    db 'OR AX, imm16: FAIL',0Dh,0Ah,'$'
-
-; PUSH/POP strings
-pesh_pass:   db 'PUSH ES: PASS',0Dh,0Ah,'$'
-pesh_fail:   db 'PUSH ES: FAIL',0Dh,0Ah,'$'
-
-pes_pass:    db 'POP ES: PASS',0Dh,0Ah,'$'
-pes_fail:    db 'POP ES: FAIL',0Dh,0Ah,'$'
-
-pcsh_pass:   db 'PUSH CS: PASS',0Dh,0Ah,'$'
-pcsh_fail:   db 'PUSH CS: FAIL',0Dh,0Ah,'$'
-
-; ADC strings
-adc1_pass:   db 'ADC r/m8, r8: PASS',0Dh,0Ah,'$'
-adc1_fail:   db 'ADC r/m8, r8: FAIL',0Dh,0Ah,'$'
-
-adc2_pass:   db 'ADC r/m16, r16: PASS',0Dh,0Ah,'$'
-adc2_fail:   db 'ADC r/m16, r16: FAIL',0Dh,0Ah,'$'
-
-adc3_pass:   db 'ADC r8, r/m8: PASS',0Dh,0Ah,'$'
-adc3_fail:   db 'ADC r8, r/m8: FAIL',0Dh,0Ah,'$'
-
-adc4_pass:   db 'ADC r16, r/m16: PASS',0Dh,0Ah,'$'
-adc4_fail:   db 'ADC r16, r/m16: FAIL',0Dh,0Ah,'$'
-
-adc5_pass:   db 'ADC AL, d8: PASS',0Dh,0Ah,'$'
-adc5_fail:   db 'ADC AL, d8: FAIL',0Dh,0Ah,'$'
-
-adc6_pass:   db 'ADC AX, d16: PASS',0Dh,0Ah,'$'
-adc6_fail:   db 'ADC AX, d16: FAIL',0Dh,0Ah,'$'
-
-; SBB strings
-sbb1_pass:   db 'SBB r/m8, r8: PASS',0Dh,0Ah,'$'
-sbb1_fail:   db 'SBB r/m8, r8: FAIL',0Dh,0Ah,'$'
-
-sbb2_pass:   db 'SBB r/m16, r16: PASS',0Dh,0Ah,'$'
-sbb2_fail:   db 'SBB r/m16, r16: FAIL',0Dh,0Ah,'$'
-
-sbb3_pass:   db 'SBB r8, r/m8: PASS',0Dh,0Ah,'$'
-sbb3_fail:   db 'SBB r8, r/m8: FAIL',0Dh,0Ah,'$'
-
-sbb4_pass:   db 'SBB r16, r/m16: PASS',0Dh,0Ah,'$'
-sbb4_fail:   db 'SBB r16, r/m16: FAIL',0Dh,0Ah,'$'
-
-sbb5_pass:   db 'SBB AL, d8: PASS',0Dh,0Ah,'$'
-sbb5_fail:   db 'SBB AL, d8: FAIL',0Dh,0Ah,'$'
-
-sbb6_pass:   db 'SBB AX, d16: PASS',0Dh,0Ah,'$'
-sbb6_fail:   db 'SBB AX, d16: FAIL',0Dh,0Ah,'$'
-
-; AND strings
-and1_pass:   db 'AND r/m8, r8: PASS',0Dh,0Ah,'$'
-and1_fail:   db 'AND r/m8, r8: FAIL',0Dh,0Ah,'$'
-
-and2_pass:   db 'AND r/m16, r16: PASS',0Dh,0Ah,'$'
-and2_fail:   db 'AND r/m16, r16: FAIL',0Dh,0Ah,'$'
-
-and3_pass:   db 'AND r8, r/m8: PASS',0Dh,0Ah,'$'
-and3_fail:   db 'AND r8, r/m8: FAIL',0Dh,0Ah,'$'
-
-and4_pass:   db 'AND r16, r/m16: PASS',0Dh,0Ah,'$'
-and4_fail:   db 'AND r16, r/m16: FAIL',0Dh,0Ah,'$'
-
-and5_pass:   db 'AND AL, imm8: PASS',0Dh,0Ah,'$'
-and5_fail:   db 'AND AL, imm8: FAIL',0Dh,0Ah,'$'
-
-and6_pass:   db 'AND AX, imm16: PASS',0Dh,0Ah,'$'
-and6_fail:   db 'AND AX, imm16: FAIL',0Dh,0Ah,'$'
-
-; DAA/DAS/AAA/AAS strings
-daa_pass:    db 'DAA: PASS',0Dh,0Ah,'$'
-daa_fail:    db 'DAA: FAIL',0Dh,0Ah,'$'
-
-das_pass:    db 'DAS: PASS',0Dh,0Ah,'$'
-das_fail:    db 'DAS: FAIL',0Dh,0Ah,'$'
-
-aaa_pass:    db 'AAA: PASS',0Dh,0Ah,'$'
-aaa_fail:    db 'AAA: FAIL',0Dh,0Ah,'$'
-
-aas_pass:    db 'AAS: PASS',0Dh,0Ah,'$'
-aas_fail:    db 'AAS: FAIL',0Dh,0Ah,'$'
-
-; SUB strings
-sub1_pass:   db 'SUB r/m8, r8: PASS',0Dh,0Ah,'$'
-sub1_fail:   db 'SUB r/m8, r8: FAIL',0Dh,0Ah,'$'
-
-sub2_pass:   db 'SUB r/m16, r16: PASS',0Dh,0Ah,'$'
-sub2_fail:   db 'SUB r/m16, r16: FAIL',0Dh,0Ah,'$'
-
-sub3_pass:   db 'SUB r8, r/m8: PASS',0Dh,0Ah,'$'
-sub3_fail:   db 'SUB r8, r/m8: FAIL',0Dh,0Ah,'$'
-
-sub4_pass:   db 'SUB r16, r/m16: PASS',0Dh,0Ah,'$'
-sub4_fail:   db 'SUB r16, r/m16: FAIL',0Dh,0Ah,'$'
-
-sub5_pass:   db 'SUB AL, d8: PASS',0Dh,0Ah,'$'
-sub5_fail:   db 'SUB AL, d8: FAIL',0Dh,0Ah,'$'
-
-sub6_pass:   db 'SUB AX, d16: PASS',0Dh,0Ah,'$'
-sub6_fail:   db 'SUB AX, d16: FAIL',0Dh,0Ah,'$'
-
-; XOR strings
-xor1_pass:   db 'XOR r/m8, r8: PASS',0Dh,0Ah,'$'
-xor1_fail:   db 'XOR r/m8, r8: FAIL',0Dh,0Ah,'$'
-
-xor2_pass:   db 'XOR r/m16, r16: PASS',0Dh,0Ah,'$'
-xor2_fail:   db 'XOR r/m16, r16: FAIL',0Dh,0Ah,'$'
-
-xor3_pass:   db 'XOR r8, r/m8: PASS',0Dh,0Ah,'$'
-xor3_fail:   db 'XOR r8, r/m8: FAIL',0Dh,0Ah,'$'
-
-xor4_pass:   db 'XOR r16, r/m16: PASS',0Dh,0Ah,'$'
-xor4_fail:   db 'XOR r16, r/m16: FAIL',0Dh,0Ah,'$'
-
-xor5_pass:   db 'XOR AL, imm8: PASS',0Dh,0Ah,'$'
-xor5_fail:   db 'XOR AL, imm8: FAIL',0Dh,0Ah,'$'
-
-xor6_pass:   db 'XOR AX, imm16: PASS',0Dh,0Ah,'$'
-xor6_fail:   db 'XOR AX, imm16: FAIL',0Dh,0Ah,'$'
-
-; CMP strings
-cmp1_pass:   db 'CMP r/m8, r8: PASS',0Dh,0Ah,'$'
-cmp1_fail:   db 'CMP r/m8, r8: FAIL',0Dh,0Ah,'$'
-
-cmp2_pass:   db 'CMP r/m16, r16: PASS',0Dh,0Ah,'$'
-cmp2_fail:   db 'CMP r/m16, r16: FAIL',0Dh,0Ah,'$'
-
-cmp3_pass:   db 'CMP r8, r/m8: PASS',0Dh,0Ah,'$'
-cmp3_fail:   db 'CMP r8, r/m8: FAIL',0Dh,0Ah,'$'
-
-cmp4_pass:   db 'CMP r16, r/m16: PASS',0Dh,0Ah,'$'
-cmp4_fail:   db 'CMP r16, r/m16: FAIL',0Dh,0Ah,'$'
-
-cmp5_pass:   db 'CMP AL, d8: PASS',0Dh,0Ah,'$'
-cmp5_fail:   db 'CMP AL, d8: FAIL',0Dh,0Ah,'$'
-
-cmp6_pass:   db 'CMP AX, d16: PASS',0Dh,0Ah,'$'
-cmp6_fail:   db 'CMP AX, d16: FAIL',0Dh,0Ah,'$'
-
-; PUSH/POP SS/DS strings
-pssh_pass:   db 'PUSH SS: PASS',0Dh,0Ah,'$'
-pssh_fail:   db 'PUSH SS: FAIL',0Dh,0Ah,'$'
-
-pss_pass:    db 'POP SS: PASS',0Dh,0Ah,'$'
-pss_fail:    db 'POP SS: FAIL',0Dh,0Ah,'$'
-
-pdsh_pass:   db 'PUSH DS: PASS',0Dh,0Ah,'$'
-pdsh_fail:   db 'PUSH DS: FAIL',0Dh,0Ah,'$'
-
-pds_pass:    db 'POP DS: PASS',0Dh,0Ah,'$'
-pds_fail:    db 'POP DS: FAIL',0Dh,0Ah,'$'
-
-msg_done:    db 0Dh,0Ah,'All tests complete.',0Dh,0Ah,'$'
